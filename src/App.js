d4463acbdd6afba3b59e56881b8f0b465881c6ba
elakkiya-p <elakkiyaeee007@gmail.com>
2024-03-19 20:54:43 +0530

    addon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [users,setUsers]=useState([])
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setUsers(json))
+
+  },[])
+  const mapdata=()=>{
+     //map function
+   // let mappedArray=users.map((user)=> user.id *2)
+  // console.log(mappedArray)
+  //filter function
+  let filterData=users.filter((user)=>{
+    return user.id<=5
+  })
+    console.log(filterData)
+
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>user</h1>
+      <div className="card">
+        {users.map((user,index)=>(
+        <div className="card-inner" key={index}>
+         
+<span>{user.name}</span>
+<span>{user.username}</span>
+        </div>
+        )
+        )
+}
+    <button onClick={mapdata}>setmappedarray</button>
+
+      </div>
+      
+          </div>
   );
 }
 
